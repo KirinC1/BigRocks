842d1929b9426c1ecf4ca96ed34f812bff6e214b
Elekeko420 <kchanteloup22@dtechhs.org>
2019-11-06 22:26:40 +0000

    RIP

--- minecraft-master/js/entities/pixel.js
+++
@@ -1,7 +0,0 @@
-class Pixel {
-  constructor(initialType) {
-    this.initialType = initialType;
-    this.position = [undefined, undefined]; // (x,y)
-    this.type = initialType; // string
-  }
-}
--- minecraft-master/js/mainmenu.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function () {
-  $('#btn-menu-instructions').click((e)=> {
-      $("#gameModal").modal("show");
-  });
-});
